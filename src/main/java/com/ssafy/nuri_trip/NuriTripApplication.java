24ccbdb691280cf1aa35eb91040a492eee52509e
Yuunhye <pyl66600@gmail.com>
2024-11-25 00:49:54 +0900

    init

---
+++ src/main/java/com/ssafy/nuri_trip/NuriTripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nuri_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuriTripApplication.class, args);
+	}
+
+}
