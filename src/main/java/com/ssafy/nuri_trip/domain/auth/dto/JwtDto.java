c19e8c7a9daf8c3f3081cb51956c0c550e6a41d9
Yuunhye <pyl66600@gmail.com>
2024-11-25 14:33:28 +0900

    #1 [feat] implement signin feature

---
+++ src/main/java/com/ssafy/nuri_trip/domain/auth/dto/JwtDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.nuri_trip.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtDto {
+    private Long id;
+}
--- src/main/java/com/ssafy/nuri_trip/domain/auth/service/AuthService.java
+++ src/main/java/com/ssafy/nuri_trip/domain/auth/service/AuthService.java
@@ -5,7 +5,9 @@ import com.ssafy.nuri_trip.domain.auth.dto.LoginRes;
 import com.ssafy.nuri_trip.domain.user.dto.User;
 import com.ssafy.nuri_trip.domain.user.repository.UserRepository;
 import com.ssafy.nuri_trip.global.common.BaseException;
+import com.ssafy.nuri_trip.global.utils.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import static com.ssafy.nuri_trip.global.common.BaseResponseStatus.FAILED_TO_LOGIN;
@@ -14,13 +16,18 @@ import static com.ssafy.nuri_trip.global.common.BaseResponseStatus.FAILED_TO_LOG
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public LoginRes login(LoginReq loginReq) throws BaseException{
 
         String userId = loginReq.getId();
         User user = userRepo.selectByUserId(userId);
-        if(loginReq.getPassword().equals(user.getPassword())){
-            return new LoginRes(user.getId(), "jwt", user.getName(), user.getProfileImg(), user.getStrengthLevel());
+        String encodedPassword = passwordEncoder.encode(loginReq.getPassword());
+        if(user!= null && encodedPassword.equals(user.getPassword())){
+            Long id = user.getId();
+            String jwtToken = jwtService.createJwt(id);
+            return new LoginRes(user.getId(), jwtToken, user.getName(), user.getProfileImg(), user.getStrengthLevel());
         }else{
             throw new BaseException(FAILED_TO_LOGIN);
         }
