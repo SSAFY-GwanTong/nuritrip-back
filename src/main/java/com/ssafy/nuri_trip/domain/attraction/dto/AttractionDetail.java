61488a931175d8759b695f100c33138204e84096
Yuunhye <pyl66600@gmail.com>
2024-11-26 14:52:42 +0900

    #17 [feat] 관광지, 지역 관련 기능 구현

--- src/main/java/com/ssafy/nuri_trip/domain/attraction/dto/AttractionDetail.java
+++ src/main/java/com/ssafy/nuri_trip/domain/attraction/dto/AttractionDetail.java
@@ -2,18 +2,20 @@ package com.ssafy.nuri_trip.domain.attraction.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Attraction {
+@RequiredArgsConstructor
+public class AttractionDetail {
     private int contentId;
-    private double latitude;
-    private double longitude;
     private String title;
     private String img;
+    private String tel;
     private String address1;
     private String address2;
-    private String contentType;
+    private String overview;
+    private int likes;
 }
--- src/main/java/com/ssafy/nuri_trip/domain/attraction/service/AttractionService.java
+++ src/main/java/com/ssafy/nuri_trip/domain/attraction/service/AttractionService.java
@@ -1,6 +1,8 @@
 package com.ssafy.nuri_trip.domain.attraction.service;
 
 import com.ssafy.nuri_trip.domain.attraction.dto.Attraction;
+import com.ssafy.nuri_trip.domain.attraction.dto.AttractionDetail;
+import com.ssafy.nuri_trip.domain.attraction.dto.Gugun;
 import com.ssafy.nuri_trip.domain.attraction.repository.AttractionRepository;
 import com.ssafy.nuri_trip.global.common.BaseException;
 import lombok.RequiredArgsConstructor;
@@ -8,40 +10,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.ssafy.nuri_trip.global.common.BaseResponseStatus.*;
+import static com.ssafy.nuri_trip.global.common.BaseResponseStatus.ATTRACTION_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class AttractionService {
-    private final AttractionRepository attractionRepository;
+    private final AttractionRepository attractionRepo;
 
-    /**
-     * 특정 관광지를 ID로 조회
-     */
-    public Attraction getAttractionById(int contentId) throws BaseException {
-        Attraction attraction = attractionRepository.selectAttractionById(contentId);
+    public AttractionDetail getAttractionById(int contentId) throws BaseException {
+        AttractionDetail attraction = attractionRepo.selectAttractionById(contentId);
         if (attraction == null) {
             throw new BaseException(ATTRACTION_NOT_FOUND);
         }
         return attraction;
     }
 
-    /**
-     * 구군 코드로 관광지 리스트 조회
-     */
-    public List<Attraction> getAttractionsByGugunCode(int gugunCode) throws BaseException {
-        List<Attraction> attractions = attractionRepository.selectAttractionsByGugunCode(gugunCode);
-        if (attractions == null || attractions.isEmpty()) {
-            throw new BaseException(ATTRACTION_NOT_FOUND);
-        }
-        return attractions;
+    public List<Gugun> getGuguns(int sidoCode) throws BaseException{
+        List<Gugun> guguns = attractionRepo.selectGugunBySidoCode(sidoCode);
+        return guguns;
     }
 
-    /**
-     * 특정 조건으로 관광지 조회
-     */
+
     public List<Attraction> getAttractionsByConditions(Integer sido, Integer gugun, Integer contentTypeId) throws BaseException {
-        List<Attraction> attractions = attractionRepository.selectAttractionsByConditions(sido, gugun, contentTypeId);
+        List<Attraction> attractions = attractionRepo.selectAttractionsByConditions(sido, gugun, contentTypeId);
         if (attractions == null || attractions.isEmpty()) {
             throw new BaseException(ATTRACTION_NOT_FOUND);
         }
